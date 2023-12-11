d0bc92de415bed03f062fb957bc9c0d5a8e74fbe
Shaposhnikov Alexander <firstremont@gmail.com>
2023-12-11 18:52:51 +0800

    11dec

--- src/blog/views.py
+++ src/blog/views.py
@@ -12,10 +12,6 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-#def post_new(request):
-    #form = PostForm()
-    #return render(request, 'blog/post_edit.html', {'form': form})
-
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
