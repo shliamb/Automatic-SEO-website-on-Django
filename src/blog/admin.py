b20dd18482c2a15038680b8a5a1639aa79074506
Shaposhnikov Alexander <firstremont@gmail.com>
2023-12-16 19:40:04 +0800

    16dec2

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    list_filter = ('title', 'created_date', 'published_date', 'author')
+    search_fields = ('title', 'author')
+    #prepopulated_fields = {'author': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published_date'
+    ordering = ['author', 'published_date']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_date', 'approved_comment')
+    list_filter = ('post', 'author', 'created_date', 'approved_comment')
+    search_fields = ('post', 'author')
+    #prepopulated_fields = {'author': ('title',)}
+    #raw_id_fields = ('author',)
+    date_hierarchy = 'created_date'
+    ordering = ['author', 'created_date']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+
+
 
