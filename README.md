68d9b1ba939e9d6335174e4dcfa1ee8b3b9103d4
Shaposhnikov Alexander <firstremont@gmail.com>
2023-12-27 21:56:59 +0800

    27dec3

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # www.1rmaster.ru
 I'm redesigning my website for Django 8 dec 2023
 
-#RU
+# RU
 Я переделываю свой сайт. Сайт работает с 2009 года. Раньше был сайтом моей мастерской по ремонту ноутбуков и видеокарт. Пришло время перемен. Сейчас основой ему будет фреймворк Django на минималках. Никакого украшения и фронтенда как такового.
 
 --------------------------------------------------------------------------------
 
-#EN
+# EN
 I'm redesigning my website. The site has been operating since 2009. It used to be the site of my laptop and video card repair shop. It's time for a change. Now it will be based on the Django framework on minimalks. No decoration or frontend as such.
--- mainweb/models.py
+++ mainweb/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Страница
+class Page(models.Model):
+    title = models.CharField(max_length=250) # Заголовок статьи на странице, если нужна будет ссылка с названием
+    description = models.TextField()
+    keywords = models.CharField(max_length=250) 
+    seotitle = models.CharField(max_length=250) # заголовок title страницы в html
+    slug = models.SlugField(max_length=250, unique=True) # url страницы
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # автора берет из таблицы User встроенной ко связи ключа для нормализации базы
+    body = models.TextField() # Основной текст
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Page(models.Model):
+    title = models.CharField(max_length=250)
+
+
+def __str__(self):
+    return self.title
