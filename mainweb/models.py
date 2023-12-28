5192674f7cee914ea772e7492262b72ff0330471
Shaposhnikov Alexander <firstremont@gmail.com>
2023-12-28 12:44:39 +0800

    28dec23models

--- mainweb/models.py
+++ mainweb/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Страница
+# Модиль Страницы
 class Page(models.Model):
     title = models.CharField(max_length=250) # Заголовок статьи на странице, если нужна будет ссылка с названием
     description = models.TextField()
@@ -15,9 +15,30 @@ class Page(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-class Page(models.Model):
-    title = models.CharField(max_length=250)
+    def __str__(self):
+        return self.title
 
+# Модель меню
+class MenuItem(models.Model):
+    title = models.CharField(max_length=100)
+    is_main_menu = models.BooleanField(default=True)  # Является ли элемент главным меню
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')  # Ссылка на родительский элемент
+    page = models.OneToOneField(Page, on_delete=models.CASCADE, null=True, blank=True) # Указывает на связь с моделью Page может быть null и может быть пустым, один пунктт меню к одномой странице
+    url = models.CharField(max_length=200) # Можно внешнию ссылку
+    active = models.BooleanField(default=False) # Активное меню для визуализации
+    order = models.PositiveIntegerField(default=0) # порядок отображения от 0 - 5 к примеру
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
+
+# Модель блоков  
+class Modul(models.Model): # дополнительные блоки на страницах
+    title = models.CharField(max_length=100)
+    body = models.TextField() # Основной текст модуля
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # автора берет из таблицы User встроенной ко связи ключа для нормализации базы
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
